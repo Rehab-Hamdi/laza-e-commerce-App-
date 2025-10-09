556d8dd0705c13557a17e238c5db4930dd58bef9
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-10-09 18:53:09 +0300

    routes

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String splashScreen = '/';
+  static const String loginScreen = '/login';
+  static const String registerScreen = '/register';
+  static const String otpScreen = '/otp';
+  static const String homeLayout = '/homeLayout';
+  static const String homeScreen = '/home';
+}
